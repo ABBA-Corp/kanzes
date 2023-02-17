be1c109fca4e12bd48d760db1957c768e7ed420e
Mahmud <mahmudnosirov77@gmail.com>
2023-02-17 23:38:09 +0500

    Decor website

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ cd ../
 sudo rm -r decor
 git clone https://github.com/mahmudnosirov-abba/kanzes.git
 cd decor
-git remote set-url origin git@github.com:mahmudnosirov-abba/kazes.git
+git remote set-url origin git@github.com:mahmudnosirov-abba/kanzes.git
 cd ../
