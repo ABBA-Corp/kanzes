33e7d620f52a2411cc70822e1fee2004404d0ced
Mahmud <mahmudnosirov77@gmail.com>
2023-02-18 16:16:58 +0500

    Decor website

--- backend/views.py
+++ backend/views.py
@@ -7,39 +7,39 @@ from .models import *
 class NewsView(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class NewsCommentView(viewsets.ModelViewSet):
     queryset = NewsComment.objects.all()
     serializer_class = NewsCommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class TopProductView(viewsets.ModelViewSet):
     queryset = Product.objects.filter(top = True)
     serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class SearchProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = SearchProductSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class OurWorkView(viewsets.ModelViewSet):
     queryset = OurWork.objects.all()
     serializer_class = OurWorkSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class FAQView(viewsets.ModelViewSet):
     queryset = FAQ.objects.all()
     serializer_class = FAQSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
